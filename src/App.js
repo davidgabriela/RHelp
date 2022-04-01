97479813b65d7cbc13756bb0a795870027a2c08e
GabrielaD <david.gabriela.99@gmail.com>
2022-04-01 11:44:57 +0300

    fixed error in App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import  MainPage from "./MainPage/MainPage";
+import MainPage from "./MainPage/MainPage";
+import React, { Component } from "react";
+
 function App() {
-  return (
-   <>
-      <div className="align-items-center justify-content-center">
-        <div className="w-100">
-        <Router>
-        <Switch>
-        <Route exact path="/" component={MainPage}/>
-        </Switch>
-        </Router>
-        </div>
-      </div>
-   </>
-  );
+    return (
+        <>
+            <div className='align-items-center justify-content-center'>
+                <div className='w-100'>
+                    <Router>
+                        <Switch>
+                            <Route exact path='/' component={MainPage} />
+                        </Switch>
+                    </Router>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
