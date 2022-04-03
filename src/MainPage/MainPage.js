27a447ddd5c66a5facb427c38c23aade997f28cd
GabrielaD <david.gabriela.99@gmail.com>
2022-04-03 19:33:46 +0300

    changes main page

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -3,6 +3,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 import classes from "./MainPage.css";
 
 class MainPage extends React.Component {
@@ -13,31 +16,60 @@ class MainPage extends React.Component {
                     <Row className='main-row'>
                         <Col lg={12} xl className='main-col main-map'></Col>
                         <Col lg={12} xl className='main-col main-hero'>
-                            <Container>
-                                <Row>
-                                    <Col></Col>
-                                    <Col>
-                                        <Button>English</Button>
-                                    </Col>
-                                    <Col>
-                                        <Button variant='link'>FAQ</Button>
-                                    </Col>
-                                    <Col>
-                                        <Button variant='link'>Support</Button>
-                                    </Col>
-                                    <Col>
-                                        <Button>Log In</Button>
-                                    </Col>
-                                </Row>
-                            </Container>
-                            <h1>Housing for refugees</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Suspendisse malesuada id nibh
-                                vel placerat. Pellentesque pulvinar tempus lorem
-                                eu fringilla.{" "}
-                            </p>
-                            <Button variant='primary'>Primary</Button>{" "}
+                            <Nav
+                                className='justify-content-end navigation'
+                                activeKey='/home'
+                            >
+                                <Nav.Item>
+                                    <Nav.Link href=''>
+                                        <DropdownButton
+                                            id='dropdown-basic-button'
+                                            title='English'
+                                        >
+                                            <Dropdown.Item href='#/action-1'>
+                                                French
+                                            </Dropdown.Item>
+                                            <Dropdown.Item href='#/action-2'>
+                                                German
+                                            </Dropdown.Item>
+                                            <Dropdown.Item href='#/action-3'>
+                                                Italian
+                                            </Dropdown.Item>
+                                        </DropdownButton>
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link>
+                                        <Button variant='outline-light'>
+                                            FAQ
+                                        </Button>
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link>
+                                        <Button variant='outline-light'>
+                                            Support
+                                        </Button>
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link>
+                                        <Button variant='primary'>
+                                            Log In
+                                        </Button>
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <div className='main-box'>
+                                <h1>Housing for refugees</h1>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Suspendisse malesuada id
+                                    nibh vel placerat. Pellentesque pulvinar
+                                    tempus lorem eu fringilla.{" "}
+                                </p>
+                                <Button variant='primary'>Get Started</Button>{" "}
+                            </div>
                         </Col>
                     </Row>
                 </Container>
