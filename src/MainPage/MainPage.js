7db31d99305fbc83a1d4bf5c208135ea6bd2cb72
davidgabriela <david.gabriela.99@gmail.com>
2022-04-06 17:06:57 +0300

    fixed navbar style bug

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -3,17 +3,18 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import Nav from "react-bootstrap/Nav";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
 import classes from "./MainPage.css";
 import { Link } from "react-router-dom";
+import Navbar from '../Navbar/Navbar';
 
 class MainPage extends React.Component {
     render() {
         return (
             <>
                 <Container className='main-container'>
+                    <Row>
+                        <Navbar></Navbar>
+                    </Row>
                     <Row className='main-row'>
                         <Col lg={12} xl className='main-col main-map'></Col>
                         <Col lg={12} xl className='main-col main-hero'>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,8 +5,10 @@ import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
-import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function BootstrapNavbar(props) {
     const [error, setError] = useState("");
@@ -15,76 +17,67 @@ export default function BootstrapNavbar(props) {
         setError("");
     }
     return (
-        <div>
-            <div className='row'>
-                <div className='col-md-12'>
-                    <Router>
-                        <Navbar
-                            id='navbar-container'
-                            bg='dark'
-                            variant='dark'
-                            expand='lg'
-                            sticky='top'
+        <Navbar
+            id='navbar-container'
+            bg='dark'
+            variant='dark'
+            sticky='top'
+        >
+            <Navbar.Brand href='/'>
+                React-Bootstrap
+            </Navbar.Brand>
+            <Nav>
+                {props.role === "host" ? (
+                    <Nav.Item>
+                        <Nav.Link href='/addlisting'>
+                            <Button variant='outline-light'>
+                                Add Listing
+                            </Button>
+                        </Nav.Link>
+                    </Nav.Item>
+                ) : (
+                    <div></div>
+                )}
+                <Nav.Item>
+                    <Nav.Link href=''>
+                        <DropdownButton
+                            id='dropdown-basic-button'
+                            title='English'
                         >
-                            <Navbar.Brand href='/'>
-                                React-Bootstrap
-                            </Navbar.Brand>
-                            <Nav>
-                                {props.role === "host" ? (
-                                    <Nav.Item>
-                                        <Nav.Link href='/addlisting'>
-                                            <Button variant='outline-light'>
-                                                Add Listing
-                                            </Button>
-                                        </Nav.Link>
-                                    </Nav.Item>
-                                ) : (
-                                    <div></div>
-                                )}
-                                <Nav.Item>
-                                    <Nav.Link href=''>
-                                        <DropdownButton
-                                            id='dropdown-basic-button'
-                                            title='English'
-                                        >
-                                            <Dropdown.Item href='#/action-1'>
-                                                French
-                                            </Dropdown.Item>
-                                            <Dropdown.Item href='#/action-2'>
-                                                German
-                                            </Dropdown.Item>
-                                            <Dropdown.Item href='#/action-3'>
-                                                Italian
-                                            </Dropdown.Item>
-                                        </DropdownButton>
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link>
-                                        <Button variant='outline-light'>
-                                            FAQ
-                                        </Button>
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link>
-                                        <Button variant='outline-light'>
-                                            Support
-                                        </Button>
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link href='/login'>
-                                        <Button variant='primary'>
-                                            Log In
-                                        </Button>
-                                    </Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                        </Navbar>
-                    </Router>
-                </div>
-            </div>
-        </div>
+                            <Dropdown.Item href='#/action-1'>
+                                French
+                            </Dropdown.Item>
+                            <Dropdown.Item href='#/action-2'>
+                                German
+                            </Dropdown.Item>
+                            <Dropdown.Item href='#/action-3'>
+                                Italian
+                            </Dropdown.Item>
+                        </DropdownButton>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Button variant='outline-light'>
+                            FAQ
+                        </Button>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link>
+                        <Button variant='outline-light'>
+                            Support
+                        </Button>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href='/login'>
+                        <Button variant='primary'>
+                            Log In
+                        </Button>
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Navbar>
     );
 }
