d5b5fb12e2e048bbeab725082a711033889b5630
GabrielaD <david.gabriela.99@gmail.com>
2022-04-13 10:06:40 +0300

    fixing cards...

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -6,11 +6,7 @@ const Card = (props) => {
     return (
         <div className='card text-center shadow'>
             <div className='overflow'>
-                <img
-                    src={props.imgsrc}
-                    alt='Image 1'
-                    className='card-img-top'
-                />
+                <img src={props.imgsrc} alt='' className='card-img-top' />
             </div>
 
             <div className='card-body text-dark'>
